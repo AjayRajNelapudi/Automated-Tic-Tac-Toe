f4ea8b1f9a9524f9227ad42ba265e5082b0a66fc
Ajay Raj <najayraj.16.cse@anits.edu.in>
2017-10-02 14:02:00 +0530

    Edges Handled

---
+++ Judge.java
@@ -0,0 +1,72 @@
+package ttt;
+
+public class Judge
+{
+	char field[];
+	int pattern[][]= {{0,1,2},{3,4,5},{6,7,8},
+					  {0,3,6},{1,4,7},{2,5,8},
+					  {0,4,8},{2,4,6}		};
+	char winnerLetter;
+	AlgorithmDeAjay ADA;
+	
+	Judge(char field[], AlgorithmDeAjay ADA)
+	{
+		this.field=field;
+		this.ADA=ADA;
+	}
+	
+	public void checkWinner()
+	{
+		for(int i=0;i<8;i++)
+		{
+			if(patternCheck(pattern[i][0], pattern[i][1], pattern[i][2]))
+			{
+				declareWinner();
+			}
+			else if(drawCheck())
+			{
+				gameDraw();
+			}
+		}
+	}
+	
+	public boolean patternCheck(int x, int y, int z)
+	{
+		if(field[x]=='X' && field[y]=='X' && field[z]=='X')
+		{
+			winnerLetter='X';
+			return true;
+		}
+		else if(field[x]=='O' && field[y]=='O' && field[z]=='O')
+		{
+			winnerLetter='O';
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void declareWinner()
+	{
+		System.out.println("The winner is " + winnerLetter);
+		System.exit(0);
+	}
+	
+	public boolean drawCheck()
+	{
+		for(int i=0;i<9;i++)
+		{
+			if(ADA.isDigit(field[i]))
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public void gameDraw()
+	{
+		System.out.println("Game Draw");
+		System.exit(0);
+	}
+}
